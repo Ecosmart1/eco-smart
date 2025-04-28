fd3d8478ba390a0901a1c947247985c39eceda4f
ElShuky <moyarce17@alumnos.utalca.cl>
2025-04-28 03:26:44 -0400

    arreglo script borrar un punto

--- start-ecosmart.ps1
+++ start-ecosmart.ps1
@@ -3,7 +3,7 @@ Set-Location "ecosmart backend flask"
 if (-not (Test-Path "venv")) {
     python -m venv venv
 }
-. .\venv\Scripts\Activate.ps1
+.\venv\Scripts\Activate.ps1
 python.exe -m pip install --upgrade pip
 pip install pandas
 pip install -r requirements.txt
